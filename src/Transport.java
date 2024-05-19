d69e0e28a61418a5ac0378cfee0b801c46285992
AndreyTerechenko <Dronka97@yandex.ru>
2024-05-19 21:39:42 +0300

    oop

---
+++ src/Transport.java
@@ -0,0 +1,29 @@
+public abstract class Transport implements EngineeringWorks {
+    private String modelName;
+    private int wheelsCount;
+
+    public Transport(String modelName, int wheelsCount) {
+        this.modelName = modelName;
+        this.wheelsCount = wheelsCount;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public int getWheelsCount() {
+        return wheelsCount;
+    }
+
+    public void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+
+    @Override
+    public void service() {
+        System.out.println("Обслуживаем " + getModelName());
+        for (int i = 0; i < getWheelsCount(); i++) {
+            updateTyre();
+        }
+    }
+}
